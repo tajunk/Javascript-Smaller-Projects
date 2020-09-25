f058ee0142f2abe89fbe5e00c67f512eb6f83b9b
tajunk <tajunk@gmail.com>
2020-09-24 19:09:08 -0600

    4-6 complete

--- 04-Navbar/initial/app.js
+++ 04-Navbar/initial/app.js
@@ -3,3 +3,22 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    // console.log(links.classList);
+    // console.log(links.classList.contains("links"));
+
+    // if(links.classList.contains("show-links")){
+    //     links.classList.remove("show-links")
+    // }
+    // else{
+    //     links.classList.add("show-links");
+    // }
+
+    links.classList.toggle("show-links");
+});
+
+// When setting up @media css remember to set height: auto on .links css, or else .show-links will break it
